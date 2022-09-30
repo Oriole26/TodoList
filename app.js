ee12a8fe15ecce4f98fc126d0a80c7887179b663
Hoàng Oanh <hoangoanhhbg@gmail.com>
2022-09-30 16:03:15 +0700

    Add check item&filter todo

--- app.js
+++ app.js
@@ -4,46 +4,37 @@ const addBtn = document.querySelector('.todo-btn');
 
 function addTodo(todo) {
 	const li = document.createElement('li');
-	
-  
 	li.setAttribute('class', 'todo-item');
 	li.innerHTML = `<input type="checkbox" class="todo-item__checkbox" >
 					<span>${todo.text}</span>
 					<i class="icon-delete fa-solid fa-trash" ></i>
-					`
-	
-
-		
+					`	
 	list.appendChild(li)
-	
+	//When click checkbox toggle class 'completed' 
 	const span = document.querySelectorAll('.todo-item span')
-	console.log(span)
 	const checkbox = document.querySelectorAll('.todo-item__checkbox');
-	Array.from(checkbox).forEach((checkBtn)=> {
-		checkBtn.addEventListener('click', ()=> {
-			span.classList.add('completed')
-			// console.log(span.classList.add('completed'))
-			
-		})
+	checkbox.forEach((checkBtn,index) => {
+		checkBtn.onclick = function() {
+			span[index].classList.toggle('completed')
+		}
 	})
+    //Delete item when click
 	const deleteBtn = document.querySelectorAll('.icon-delete')
-	Array.from(deleteBtn).forEach((deleteButton) => {
-		deleteButton.addEventListener('click', () => {
+	 deleteBtn.forEach((deleteButton) => {
+		deleteButton.onclick = function() {
 			deleteButton.parentNode.remove();
-		});
+		}
 	  });
 			
 	}
-
-
-
+	//Add new item when click Add Btn
 	addBtn.onclick = function() {
 		const text = input.value.trim()
 		if(	text != '')
 		addTodo({text})
 		input.value= ''
 	}
-
+	//Add new item when press 'Enter'
 	input.onkeydown = function(e) {
 		const text = input.value.trim()
 		if(e.keyCode == '13' && text != '') {
@@ -51,94 +42,32 @@ function addTodo(todo) {
 		    input.value= ''
 
 		}
-		
 	}
+//filter todo all/active/complete
+const filterBtns = document.querySelectorAll('.todo-list__title span');
+const listItem = document.querySelectorAll('.todo-list');
 
-
-
+for(i = 0; i<filterBtns.length; i++) {
+	filterBtns[i].onclick = (e) => {
+		const filter = e.target.dataset.filter;
+		console.log(filter)
+		listItem.forEach((todo)=>{
+			if(filter === 'All'){
+				todo.style.display = 'block';
+				// filterBtns.classList.add('active')
+			}
+			else {
+				if(todo.classList.contains(filter)){
 	
-		// const listItem = document.querySelectorAll('li')
-		
-		// const todos = []
+					todo.style.display = 'block';
+					
+				}
+				else {
+					todo.style.display = 'none';
 	
-		// listItem.forEach((item) => {
-		// 	todos.push({
-		// 		text: item.querySelector('span').innerHTML,
-		// 		completed: item.classList.contains('completed')
-				
-		// 	})
-		// })
+				}
+			}
+		})
+	}
 	
-
-
-// const todoList = document.querySelector('.todo-list');
-// const input = document.querySelector('.todo-input');
-// const addBtn = document.querySelector('.todo-btn');
-// const sort = document.querySelector('.todo-list__title span');
-// const title = document.querySelector('.todo-item span')
-// const checkbox = document.getElementById('#todo-item__checkbox');
-// const deleteBtn = document.querySelector('i')
-// // checkbox.checked = true;
-
-// function addTodo(todo) {
-// 	const todoItem = document.createElement('li');
-// 	todoItem.setAttribute('class',todo.completed ? 'todo-item completed' : 'todo-item')
-// 	todoItem.innerHTML = `  <input type="checkbox" id="todo-item__checkbox">
-// 							<span>${todo.text}</span>
-// 							<i class="icon-delete fa-solid fa-trash"></i>
-// 						`
-								
-// 	todoList.appendChild(todoItem);
-// 	updateTodo()
-	   
-// }
-
-
-// function HandleEvent() {
-// 	addBtn.addEventListener('click', (e) => {
-	
-// 		const text = input.value.trim();
-// 		text != '' ? addTodo({ text, completed: false }) : undefined
-// 		input.value = '';
-	
-// 	})
-// 	input.addEventListener('keypress',function(e) {
-// 		if(e.keyCode == '13') {
-// 			const text = input.value.trim();
-// 			text != '' ? addTodo({ text, completed: false }) : undefined
-// 			input.value = '';
-// 		}
-// 	})
-// 	deleteBtn.addEventListener('click', (e)=> {
-// 		e.target.parentElement.remove();
-// 		updateTodo()
-// 	})	
-// }
-
-// HandleEvent()
-// function updateTodo() {
-//     const liItem= document.querySelectorAll('todo-item')
-// 	const  todos = [];
-// 	liItem.forEach((item) => {
-// 		todos.push({
-// 			text: item.querySelector('span').innerHTML,
-// 			completed: item.classList.contains('completed'),
-// 		})
-// 	})
-// }
-
-
-
-
-
-//  checkbox.addEventListener('click', (e) => {
-// 	console.log(checkbox);
-
-
-//  })
-// checkbox.addEventListener('change', ()=> {
-//    if(this.checked) {
-// 	 title.classList.add('completed')
-// 	 console.log("OK");
-//    }
-// })
+}
